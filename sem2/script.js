91f047d59a91bf50fb6a158e112e6e07cfdf4c2a
IrinaSavosh <irina_savosh@mail.ru>
2024-07-08 05:47:32 +0300

    sem2 + HW2

---
+++ sem2/script.js
@@ -0,0 +1,12 @@
+// alert(`Любой текст внутри косых кавычек`);
+// prompt(`123`);
+// confirm(`gfhjkm`);
+// let personName = prompt('Как вас зовут?');
+// alert(`Добро пожаловать на сайт ${personName}`);
+// confirm(`Проверка: ${personName}`);
+
+let x = 10;
+let y = 15;
+let max = (x > y) ? y : x;   // (если х больше у) ? если х больше у выводим у : если х меньше у, выводим х
+alert(max);
+
