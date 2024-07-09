152ca693423436af9b635add3479c6084c4becb8
IrinaSavosh <irina_savosh@mail.ru>
2024-07-09 22:34:51 +0300

    sem3

---
+++ sem3/script.js
@@ -0,0 +1,13 @@
+//два варианта функций
+
+function one(x) {
+	return x ** 2;
+}
+
+const two = (x) => x ** 2;
+
+
+const summaNumbers = (num1, num2, num3) => {
+   console.log(Number(num1) + Number(num2) + Number(num3));
+}
+summaNumbers(1, '2', "3");
