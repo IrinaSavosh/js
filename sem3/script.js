79b143dc2900923fd34d080615cb58c8e3de34e6
IrinaSavosh <irina_savosh@mail.ru>
2024-07-12 23:22:41 +0300

    HW4

--- sem3/script.js
+++ sem3/script.js
@@ -1,13 +1,29 @@
 //два варианта функций
 
-function one(x) {
-	return x ** 2;
-}
+// function one(x) {
+// 	return x ** 2;
+// }
 
-const two = (x) => x ** 2;
+// const two = (x) => x ** 2;
 
 
-const summaNumbers = (num1, num2, num3) => {
-   console.log(Number(num1) + Number(num2) + Number(num3));
-}
-summaNumbers(1, '2', "3");
+// const summaNumbers = (num1, num2, num3) => {
+//    console.log(Number(num1) + Number(num2) + Number(num3));
+// }
+// summaNumbers(1, '2', "3");
+const NUMBER = prompt("Введите число");
+
+function isNumber(input) {
+   return !isNaN(+input);
+ }
+
+function wagesAfterTax(input){
+   let temp = isNumber(input);
+   confirm (temp);
+   if(temp){
+      confirm (`Ваш чистый доход составляет ${input * 0.87}`);
+   }else{
+   confirm (`Вы ввели не число`);}
+};
+
+wagesAfterTax(NUMBER);
